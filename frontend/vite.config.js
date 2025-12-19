aa2144320f30dd2366c27503b6f55a9539c64242
LeThanhHai071 <22130069@st.hcmuaf.edu.vn>
2025-12-19 23:31:39 +0700

    complete header with icon from bootstrap and css from thanhnien.vn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      // Cho phép Vite phục vụ các file từ thư mục node_modules và thư mục gốc dự án
+      allow: ['..'] 
+    }
+  }
 })
