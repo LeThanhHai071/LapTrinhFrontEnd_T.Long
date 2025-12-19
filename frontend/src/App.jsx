6828d65600df0b5852b6e96b9ea2564214241c9f
TuanAnh2K4-st <22130014@st.hcmuaf.edu.vn>
2025-12-19 07:48:05 +0700

    home.jsx and home.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./login";
+import Home from "./home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
