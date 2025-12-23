f2557a13ea8065894e64b8862d8cfdd47be5dbf7
Le Vu Hung <22130091@st.hcmuaf.edu.vn>
2025-12-23 17:16:34 +0700

    dat bao page and quang cao page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,25 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Login from "./login";
 import Home from "./home";
 import Register from "./register";
-import NewsDetail from "./newsDetail";
+import Advertise from "./advertise";
+import NewsDetail from "./newsDetail.jsx";
+import Subscription from "./Subscription.jsx";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/news/:id" element={<NewsDetail />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Navigate to="/home"/>}/>
+                <Route path="/home" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/news/:id" element={<NewsDetail/>}/>
+                <Route path="/subscription" element={<Subscription/>}/>
+                <Route path="/advertise" element={<Advertise/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
