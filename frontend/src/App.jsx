64534d7122891d5efe2813e7c126479b2f5ad04a
TuanAnh2K4-st <22130014@st.hcmuaf.edu.vn>
2026-01-03 15:56:13 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./login";
 import Home from "./home";
 import Register from "./register";
@@ -11,15 +11,17 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Navigate to="/home"/>}/>
-                <Route path="/home" element={<Home/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/register" element={<Register/>}/>
-                <Route path="/news/:id" element={<NewsDetail/>}/>
-                <Route path="/subscription" element={<Subscription/>}/>
-                <Route path="/advertise" element={<Advertise/>}/>
+                <Route path="/" element={<Navigate to="/home" />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/news/:id" element={<NewsDetail />} />
+                <Route path="/subscription" element={<Subscription />} />
+                <Route path="/advertise" element={<Advertise />} />
                 <Route path="/weather" element={<ShowWeather />} />
             </Routes>
         </BrowserRouter>
     );
+}
+
 export default App;
