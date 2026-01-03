2331967df0e9ccbb371c9d5e27e9f33c306bb7ff
TuanAnh2K4-st <22130014@st.hcmuaf.edu.vn>
2026-01-03 17:09:02 +0700

    fix login

--- frontend/src/components/TopHeader.jsx
+++ frontend/src/components/TopHeader.jsx
@@ -1,155 +1,136 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./TopHeader.css";
 
 const TopHeader = ({ onOpenMenu }) => {
+  const [user, setUser] = useState(null);
+  const [openUser, setOpenUser] = useState(false);
+  const navigate = useNavigate();
+
+  // Load user từ localStorage
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  // Đăng xuất
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    setUser(null);
+    setOpenUser(false);
+    navigate("/login");
+  };
+
   return (
     <div className="header__container">
       <div className="header__top-flex">
+
+        {/* LEFT */}
         <div className="header__tf-left">
           <div className="header__tf-menu" onClick={onOpenMenu}>
             <div className="bootstrap__icon">
               <i className="bi bi-list"></i>
             </div>
           </div>
+
           <div className="header__tf-search">
             <div className="box-search header-search">
               <input
                 className="btn-search txt-search txt-search-header"
                 placeholder="Tìm kiếm"
               />
-              <a className="submit-search btn-search-a">
+              <div className="submit-search btn-search-a">
                 <div className="bootstrap__icon">
                   <i className="bi bi-search"></i>
                 </div>
-              </a>
+              </div>
             </div>
           </div>
         </div>
 
+        {/* LOGO */}
         <h1>
-          <a href="/" title="Báo thanh niên" className="header__logo logo-fix">
+          <Link to="/" className="header__logo logo-fix">
             <img
               className="logo-thuong"
-              alt="Báo thanh niên"
               src="https://static.thanhnien.com.vn/thanhnien.vn/image/logo-40-nam-trang-chu.svg"
-              width="220px"
-              height="63px"
+              alt="Báo thanh niên"
             />
-          </a>
+          </Link>
         </h1>
 
-        <div className="header__tf-right box-user is-show-user">
-          <Link
-            to="./Advertise"
-            className="item"
-            title="Quảng cáo"
-          >
+        {/* RIGHT */}
+        <div className="header__tf-right box-user">
+
+          <Link to="/advertise" className="item">
             <span className="text">Quảng cáo</span>
             <span className="icon">
-              <div className="bootstrap__icon">
-                <i className="bi bi-megaphone-fill"></i>
-              </div>
+              <i className="bi bi-megaphone-fill"></i>
             </span>
           </Link>
 
-          <Link to="/subscription" className="item" title="Đặt báo">
+          <Link to="/subscription" className="item">
             <span className="text">Đặt báo</span>
             <span className="icon">
-              <div className="bootstrap__icon">
-                <i className="bi bi-newspaper"></i>
-              </div>
+              <i className="bi bi-newspaper"></i>
             </span>
           </Link>
 
-          <Link to="/login" className="item login login-btn" title="Đăng nhập">
-            <span className="text">Đăng nhập</span>
-            <span className="icon">
-              <div className="bootstrap__icon">
+          {/* CHƯA LOGIN */}
+          {!user && (
+            <Link to="/login" className="item login login-btn">
+              <span className="text">Đăng nhập</span>
+              <span className="icon">
                 <i className="bi bi-person-circle"></i>
-              </div>
-            </span>
-          </Link>
-          <div className="header__user user-des hidden list-action">
-            <div className="header__user-title title-user">
-              <a
-                href="https://my.thanhnien.vn/"
-                className="avatar "
-                id="__userImg"
+              </span>
+            </Link>
+          )}
+
+          {/* ĐÃ LOGIN */}
+          {user && (
+            <div className="header__user">
+              <div
+                className="header__user-title title-user"
+                onClick={() => setOpenUser(!openUser)}
               >
                 <img
                   src="https://static.thanhnien.com.vn/thanhnien.vn/image/ava_inter.png"
                   alt="avatar-user"
                 />
-              </a>
-              <span className="icon-down">
-                <svg
-                  width="18"
-                  height="19"
-                  viewBox="0 0 18 19"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M11.9104 7.46938L9.00035 10.3794L6.09035 7.46938C5.79785 7.17687 5.32535 7.17687 5.03285 7.46938C4.74035 7.76188 4.74035 8.23438 5.03285 8.52688L8.47535 11.9694C8.76785 12.2619 9.24035 12.2619 9.53285 11.9694L12.9754 8.52688C13.2679 8.23438 13.2679 7.76188 12.9754 7.46938C12.6829 7.18437 12.2029 7.17687 11.9104 7.46938Z"
-                    fill="#292929"
-                  ></path>
-                </svg>
-              </span>
-            </div>
-            <div className="header__user-content header__user__login">
-              <div className="header__uc-main">
-                <div className="header__uc-news group-commnent hidden">
-                  <div className="top">
-                    <span className="text">Bình luận mới được duyệt</span>
+                <span className="icon-down">
+                  <i className="bi bi-caret-down-fill"></i>
+                </span>
+              </div>
+
+              {/* MENU USER */}
+              <div
+                className="header__user-content"
+                style={{
+                  visibility: openUser ? "visible" : "hidden",
+                  opacity: openUser ? 1 : 0,
+                }}
+              >
+                <div className="header__uc-main">
+                  <div className="header__uc-link">
+                    <a className="item-link">Thông tin tài khoản</a>
+                    <a className="item-link">Đổi mật khẩu</a>
+                    <a className="item-link">Tin đã lưu</a>
+                    <a className="item-link">Tin đã xem</a>
                     <a
-                      href="https://my.thanhnien.vn/binh-luan"
-                      title="Xem tất cả"
-                      className="btn-remore view-more"
+                      className="item-link"
+                      style={{ color: "red", cursor: "pointer" }}
+                      onClick={handleLogout}
                     >
-                      Xem tất cả
+                      Đăng xuất
                     </a>
                   </div>
-                  <div className="mid">
-                    <div className="content-box" id="content_comment"></div>
-                  </div>
-                </div>
-                <div className="header__uc-link">
-                  <a
-                    href="https://my.thanhnien.vn/"
-                    className="item-link"
-                    title="Thông tin tài khoản"
-                  >
-                    Thông tin tài khoản
-                  </a>
-                  <a
-                    href="https://my.thanhnien.vn/doi-mat-khau"
-                    className="item-link"
-                    title="Đổi mật khẩu"
-                  >
-                    Đổi mật khẩu
-                  </a>
-                  <a
-                    href="https://my.thanhnien.vn/tin-da-luu"
-                    className="item-link"
-                    title="Tin đã lưu"
-                  >
-                    Tin đã lưu
-                  </a>
-                  <a
-                    href="https://my.thanhnien.vn/tin-da-xem"
-                    className="item-link"
-                    title="Tin đã xem"
-                  >
-                    Tin đã xem
-                  </a>
-                  <a className="item-link" title="Đăng xuất" id="logout_user">
-                    Đăng xuất
-                  </a>
                 </div>
               </div>
             </div>
-          </div>
+          )}
+
         </div>
       </div>
     </div>
