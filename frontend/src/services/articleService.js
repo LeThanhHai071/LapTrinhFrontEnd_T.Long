00c144657d8162dade4726dc202aee0ba0f07f75
(Lê Vũ Hùng) <22130091@st.hcmuaf.edu.vn>
2026-01-18 07:41:13 +0700

    Refactor article save logic in newsDetail and service
    refactor lại logic fe của saved article

--- frontend/src/services/articleService.js
+++ frontend/src/services/articleService.js
@@ -1,9 +1,25 @@
 import axios from "axios";
 
 const API_BASE_URL = "http://localhost:5000/api/auth";
+import { getUserIdFromStorage } from "../utils/authUtils.js";
 
 export const articleService = {
-    toggleSave: async (payload) => {
+    smartToggleSave: async (article, articleId) => {
+        const userId = getUserIdFromStorage();
+
+        if (!userId) {
+            throw new Error("Chưa đăng nhập");
+        }
+
+        const payload = {
+            userId: userId,
+            articleId: articleId,
+            title: article?.title,
+            imageURL: article?.content?.find(b => b.type === "image_block")?.urls || "",
+            link: window.location.href,
+            sapo: article?.sapo
+        };
+
         return await axios.post(`${API_BASE_URL}/toggle-save`, payload);
     },
 
